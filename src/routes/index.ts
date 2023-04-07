1b238b95d3195c797c0b0350ba262aeab712e1c4
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-04-07 16:16:36 +0100

    feat: add initialiseRoutes

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import * as express from 'express';
+
+/**
+ * Initialise all routes
+ */
+export default function initialiseRoutes() {
+    const router = express.Router();
+
+    router.use('/api/', require('../upload/routes'));
+    router.use('/api/', require('../health'));
+
+    return router;
+}
