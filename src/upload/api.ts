1a12613028a2e5984c97034961b0e125f2fa97fd
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-04-10 17:51:41 +0100

    feat: add dequeueAllFiles

--- src/upload/api.ts
+++ src/upload/api.ts
@@ -1,7 +1,10 @@
+import { readFile } from 'fs/promises';
 import { UploadedFile } from 'express-fileupload';
+import { StreamHistory, UploadFileQueue } from '@prisma/client';
 
-import { makeLogger } from '../logger';
 import prisma from '../prismaClient';
+import { makeLogger } from '../logger';
+import { JOB_STATUS } from './constants';
 
 const log = makeLogger(module);
 
@@ -20,7 +23,6 @@ export function saveFiles(
 
 function saveFile(file: UploadedFile) {
     const { name: filename, tempFilePath, mimetype, size, md5 } = file;
-
     log.info(
         { name: filename, mimetype, size, md5, tempFilePath },
         `(${saveFile.name}) - saving file: ${filename}`
@@ -47,8 +49,115 @@ export async function getUploads() {
     });
 }
 
-export const STATUS_BY_ID = {
-    0: 'waiting',
-    1: 'processing',
-    2: 'failed',
-} as const;
+export async function dequeueAllFiles(batchSize = 10) {
+    log.info(`(${dequeueAllFiles.name})`);
+
+    let numberOfFilesProcessed = 0;
+    let i = 0;
+    const limit = 100;
+    do {
+        const uploads = await prisma.uploadFileQueue.findMany({
+            take: batchSize,
+            where: { status: JOB_STATUS.WAITING },
+            orderBy: { id: 'asc' },
+        });
+        log.info(`(${dequeueAllFiles.name}) - found ${uploads.length} uploads to process`);
+
+        if (!uploads.length) {
+            return;
+        }
+
+        for (const uploadFile of uploads) {
+            void processFile(uploadFile);
+        }
+
+        numberOfFilesProcessed = uploads.length;
+        i++;
+        log.info({ numberOfFilesProcessed, i });
+    } while (numberOfFilesProcessed >= batchSize && i < limit);
+}
+
+async function processFile(file: UploadFileQueue) {
+    log.info('processFile - entry');
+
+    if (/^StreamingHistory*/.test(file.filename)) {
+        await insertStreamingHistory(file);
+    } else if (/^Playlist*/) {
+        // await insertPlaylist(file);
+        await markAsIgnored(file); // Mark as ignored for now
+    } else {
+        log.info('This is an unknown file');
+        await markAsIgnored(file);
+    }
+}
+
+async function insertStreamingHistory(file: UploadFileQueue) {
+    log.info({ file }, `(${insertStreamingHistory.name})`);
+
+    const fileData = await readFile(file.filePath);
+    const tracks = JSON.parse(fileData.toString());
+
+    if (!isValidStreamHistory(tracks)) {
+        throw TypeError('Invalid file format: tracks are not valid');
+    }
+
+    try {
+        await prisma.$transaction(
+            tracks.map(track =>
+                prisma.streamHistory.create({
+                    data: {
+                        trackName: track.trackName,
+                        artistName: track.artistName,
+                        msPlayed: track.msPlayed,
+                        endTime: new Date(track.endTime),
+                    },
+                })
+            )
+        );
+
+        await prisma.uploadFileQueue.update({
+            where: { id: file.id },
+            data: { status: JOB_STATUS.COMPLETE },
+        });
+    } catch (err) {
+        log.error(err, 'Error creating streaming history');
+
+        await prisma.uploadFileQueue.update({
+            where: { id: file.id },
+            data: { status: JOB_STATUS.FAILED },
+        });
+    }
+}
+
+function isValidStreamHistory(tracks: any[]): tracks is StreamHistory[] {
+    if (!Array.isArray(tracks)) return false;
+
+    for (const track of tracks) {
+        if (
+            track.trackName == undefined ||
+            track.artistName == undefined ||
+            track.endTime == undefined ||
+            track.msPlayed == undefined
+        ) {
+            log.info(track, 'Track failed validation');
+            return false;
+        }
+    }
+
+    return true;
+}
+
+async function insertPlaylist(file: UploadFileQueue) {
+    log.info({ file }, 'Inserting playlist');
+}
+
+async function markAsIgnored(file: UploadFileQueue) {
+    try {
+        await prisma.uploadFileQueue.update({
+            where: { id: file.id },
+            data: { status: JOB_STATUS.IGNORED },
+        });
+    } catch (err) {
+        log.error({ err, file }, 'Error marking file as ignored');
+    }
+}
