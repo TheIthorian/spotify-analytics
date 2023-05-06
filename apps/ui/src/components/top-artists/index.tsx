c85698a2119ef29fa69222016d424c4cc33ec006
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-06 16:46:05 +0100

    feat: add TopArtists chart

---
+++ apps/ui/src/components/top-artists/index.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { getTopArtists } from './data';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+export const CHART_OPTIONS = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Bar Chart',
+        },
+    },
+};
+
+export function TopArtists() {
+    const [topArtistsData, setTopArtistsData] = useState<Array<{ name: string; count: number }>>([]);
+    const [error, setError] = useState<String>();
+    const [errorCause, setErrorCause] = useState<String>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getTopArtists({})
+            .then(setTopArtistsData)
+            .catch(err => {
+                console.error(err);
+                setErrorCause(JSON.stringify(err.cause, null, 2));
+                setError(err.message);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    const labels = topArtistsData.map(d => d.name);
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Top Artists By Playtime',
+                data: topArtistsData.map(d => d.count),
+                backgroundColor: '#1DB954',
+            },
+        ],
+    };
+
+    if (loading) {
+        return <span>Loading...</span>;
+    }
+
+    if (error) {
+        return (
+            <div>
+                <span>{error}</span>
+                <br />
+                <pre>{errorCause}</pre>
+            </div>
+        );
+    }
+
+    return <Bar options={CHART_OPTIONS} data={data} />;
+}
--- apps/ui/src/pages/index.tsx
+++ apps/ui/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { HomeLayout } from '@/layout/home';
 import { Inter } from 'next/font/google';
 import { Nav } from 'c/nav';
 import { StreamHistory } from '@/components/stream-history';
+import { TopArtists } from '@/components/top-artists';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,6 +12,7 @@ export default function Home() {
             <Nav />
             <div className='p-2'>
                 <StreamHistory />
+                <TopArtists />
             </div>
         </HomeLayout>
     );
