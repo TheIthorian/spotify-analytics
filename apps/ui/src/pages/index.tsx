7e98d3674dbb34965b67b5d9f77f1c2e68717d82
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-28 16:47:08 +0100

    feat: add StreamHistoryStats to home page

--- apps/ui/src/pages/index.tsx
+++ apps/ui/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import { Nav } from 'c/nav';
 import { StreamHistory } from '@/components/stream-history';
 import { TopArtists } from '@/components/top-artists';
+import { StreamHistoryStats } from '@/components/stream-history-stats';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,6 +12,7 @@ export default function Home() {
         <HomeLayout>
             <Nav />
             <div className='p-2'>
+                <StreamHistoryStats />
                 <StreamHistory />
                 <TopArtists />
             </div>
