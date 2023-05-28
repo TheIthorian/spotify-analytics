64d59a989ee7300304f2baee3826617ea670f45b
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-28 16:07:22 +0100

    docs: add cache doc comment

--- apps/api/src/util/cache.ts
+++ apps/api/src/util/cache.ts
@@ -1,17 +1,28 @@
 import { makeLogger } from '../logger';
 import * as NodeCache from 'node-cache';
 
-const requestCache = new NodeCache();
+const CACHE_DURATION_IN_SECONDS = 180 as const; // 3 minutes
+const requestCache = new NodeCache({ stdTTL: CACHE_DURATION_IN_SECONDS });
 
 const log = makeLogger(module);
 
-type CachedFunction<T extends (...args: any[]) => any> = (...args: Parameters<T>) => ReturnType<T>;
+type Func = (...args: any[]) => any;
 
-export function cache<T extends (...args: any[]) => any>(fn: T): CachedFunction<T> {
+type CachedFunction<T extends Func> = (...args: Parameters<T>) => ReturnType<T>;
+
+/**
+ * Returns an instance of the input function which will utilise caching. On the first invocation, the input function will be invoked and
+ * the result cached. On subsequent invocations, the cached result will be returned.
+ * @param fn - Function to cache the result of
+ * @param cacheKey - A key to store the result with. If not provided, a JSON representation of the arguments will be used as the key.
+ * It is preferable to provide a cacheKey to avoid serialising the arguments to a JSON string.
+ * @returns Cached instance of the input function
+ */
+export function cache<T extends Func>(fn: T, cacheKey?: string): CachedFunction<T> {
     const cachedFunction: CachedFunction<T> = (...args: Parameters<T>): ReturnType<T> => {
         log.info(`Caching function ${fn.name}`);
 
-        const key = JSON.stringify({ name: fn.name, args });
+        const key = cacheKey ? fn.name + ':' + cacheKey : JSON.stringify({ name: fn.name, args });
         const cached: ReturnType<T> | undefined = requestCache.get(key);
         if (cached) {
             log.info(key, `Using cached results with key ${key} `);
