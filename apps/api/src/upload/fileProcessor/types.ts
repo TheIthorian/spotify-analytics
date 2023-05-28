469a8d93e64911c1cf4b2f556d557bceb30f5964
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-28 22:52:04 +0100

    feat: make new file processor module

---
+++ apps/api/src/upload/fileProcessor/types.ts
@@ -0,0 +1,20 @@
+export type JsonStreamHistoryRecord = {
+    ts: string;
+    username: string;
+    platform: string;
+    ms_played: number;
+    master_metadata_track_name: string;
+    master_metadata_album_artist_name: string;
+    master_metadata_album_album_name: string;
+    spotify_track_uri: string;
+    episode_name: string;
+    episode_show_name: string;
+    spotify_episode_uri: string;
+    reason_start: string;
+    reason_end: string;
+    shuffle: boolean;
+    skipped: boolean;
+    offline: boolean;
+    offline_timestamp: number;
+    incognito_mode: boolean;
+};
