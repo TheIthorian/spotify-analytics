a8b30ef1b2e343563ee7330318dbf3f673178e2e
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-28 15:51:29 +0100

    fix: add type checking to cache<T>

--- apps/api/src/util/cache.ts
+++ apps/api/src/util/cache.ts
@@ -5,22 +5,27 @@ const requestCache = new NodeCache();
 
 const log = makeLogger(module);
 
-export function cache(fn: CallableFunction) {
-    log.info('Setting cache for function', fn.name);
+type CachedFunction<T extends (...args: any[]) => any> = (...args: Parameters<T>) => ReturnType<T>;
 
-    return async function (...args: any[]) {
+export function cache<T extends (...args: any[]) => any>(fn: T): CachedFunction<T> {
+    const cachedFunction: CachedFunction<T> = (...args: Parameters<T>): ReturnType<T> => {
         log.info(`Caching function ${fn.name}`);
 
         const key = JSON.stringify({ name: fn.name, args });
-        const cached = requestCache.get(key);
+        const cached: ReturnType<T> | undefined = requestCache.get(key);
         if (cached) {
             log.info(key, `Using cached results with key ${key} `);
             return cached;
         }
 
         log.info('Cache miss. Invoking actual function');
-        const result = await fn(...args);
-        requestCache.set(key, result);
+        const result = fn(...args);
+        if (result instanceof Promise) {
+            result.then(result => requestCache.set(key, result));
+        }
+
         return result;
     };
+
+    return cachedFunction;
 }
