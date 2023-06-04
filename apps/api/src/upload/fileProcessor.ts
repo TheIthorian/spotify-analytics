0f31051ebd54aaa7f23162a39e6acf70d3665559
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-04 11:29:32 +0100

    feat: process file concurrently

--- apps/api/src/upload/fileProcessor.ts
+++ apps/api/src/upload/fileProcessor.ts
@@ -2,6 +2,7 @@ import { FileProcessor } from './fileProcessor/index';
 import prisma from '../prismaClient';
 import { makeLogger } from '../logger';
 import { JOB_STATUS } from './constants';
+import { deleteTempFile } from '../util/file';
 
 const log = makeLogger(module);
 
@@ -25,11 +26,16 @@ export async function dequeueAllFiles(batchSize = 10) {
             return;
         }
 
-        for (const uploadFile of uploads) {
-            const fileType = fileProcessor.getFileType(uploadFile.filename);
-            fileType.setSource(uploadFile);
-            await fileType.process();
-        }
+        await Promise.all(
+            uploads.map(async uploadFile => {
+                const fileType = fileProcessor.getFileType(uploadFile.filename);
+                console.log('fileType', fileType.type);
+                fileType.setSource(uploadFile);
+                await fileType.process();
+            })
+        );
+
+        await Promise.all(uploads.map(async uploadFile => await deleteTempFile(uploadFile.filePath)));
 
         numberOfFilesProcessed = uploads.length;
         loopIndex++;
