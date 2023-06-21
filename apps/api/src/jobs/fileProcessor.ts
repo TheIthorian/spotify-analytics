eef1664a2e48f585fc0533b3cd06fc9bf900b322
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-21 18:59:54 +0100

    feat: change read strat for sqlite

--- apps/api/src/jobs/fileProcessor.ts
+++ apps/api/src/jobs/fileProcessor.ts
@@ -1,3 +1,4 @@
+import config from 'config';
 import { makeLogger } from '../logger';
 import { dequeueAllFiles } from '../upload/fileProcessor';
 
@@ -11,7 +12,11 @@ function dequeue() {
     log.info({ start: new Date() }, 'Processing batch');
 
     const batchSize = 10;
-    dequeueAllFiles(batchSize)
+    dequeueAllFiles(batchSize, {
+        validateFields: false,
+        readStrategy:
+            config.databaseType === 'sqlite' ? dequeueAllFiles.ReadStrategy.ReadFileAsync : dequeueAllFiles.ReadStrategy.StreamFile,
+    })
         .then(() => log.info('Batch complete'))
         .catch(err => log.error(err));
 }
