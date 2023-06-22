1ffdc1be452a1524288994dac7dfc71a8b9345be
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-22 17:30:25 +0100

    feat: use mui in nav

--- apps/ui/src/components/nav.tsx
+++ apps/ui/src/components/nav.tsx
@@ -1,3 +1,31 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const i18n = {
+    login: 'Login',
+    appTitle: 'Spotify Analytics',
+};
+
 export function Nav() {
-    return <div className='w-100 bg-slate-600 p-4 text-lg'>Nav</div>;
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position='static'>
+                <Toolbar>
+                    <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+                        {i18n.appTitle}
+                    </Typography>
+                    <Button color='inherit'>{i18n.login}</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
