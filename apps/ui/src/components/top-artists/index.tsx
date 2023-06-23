6915313f6223ea4865ce09f039278fa0b38b9c98
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-23 19:04:10 +0100

    feat: improve style of chart

--- apps/ui/src/components/top-artists/index.tsx
+++ apps/ui/src/components/top-artists/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { getTopArtists } from './data';
+import { Card } from '@mui/material';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
