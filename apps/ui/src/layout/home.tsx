19636e4c7acb6739be0346f494f6435c1d86763b
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-23 18:54:44 +0100

    feat: use mui background

--- apps/ui/src/layout/home.tsx
+++ apps/ui/src/layout/home.tsx
@@ -1,9 +1,15 @@
+import { Box } from '@mui/material';
 import { ReactNode } from 'react';
 
 export function HomeLayout({ children }: { children: ReactNode }) {
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between'>
-            <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>{children}</div>
-        </main>
+        <Box
+            sx={{
+                width: '100%',
+                height: '100%',
+            }}
+        >
+            {children}
+        </Box>
     );
 }
