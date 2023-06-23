c1da3b71f7a22b10332662808ee57c4b7911b367
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-23 22:32:36 +0100

    feat: add file uploader

--- apps/ui/src/pages/index.tsx
+++ apps/ui/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 import { HomeLayout } from '@/layout/home';
 import { Inter } from 'next/font/google';
 
@@ -6,19 +8,86 @@ import { StreamHistory } from '@/components/stream-history';
 import { TopArtists } from '@/components/top-artists';
 import { StreamHistoryStats } from '@/components/stream-history-stats';
 
-import { Stack } from '@mui/material';
+import { Box, Button, Card, Stack, Typography } from '@mui/material';
+import { CONFIG } from '@/config';
+import { UploadFiles } from '@/components/upload-file';
 
 const inter = Inter({ subsets: ['latin'] });
 
+async function getUserDetails() {
+    const res = await fetch(CONFIG.API_BASE + '/me', {});
+
+    if (!res.ok) {
+        throw new Error('Error fetching data', { cause: await res.json() });
+    }
+
+    return res.json();
+}
+
 export default function Home() {
+    const [userDetails, setUserDetails] = React.useState(null);
+
+    React.useEffect(() => {
+        getUserDetails()
+            .then(setUserDetails)
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+    console.log(userDetails);
+
+    if (!userDetails) {
+        return (
+            <HomeLayout>
+                <Nav />
+                <p>Loading...</p>
+            </HomeLayout>
+        );
+    }
+
     return (
         <HomeLayout>
             <Nav />
             <Stack sx={{ padding: 2 }} spacing={2}>
-                <StreamHistoryStats />
-                <StreamHistory />
-                <TopArtists />
+                {userDetails.streamHistoryRecordCount > 0 ? <Analytics /> : <GetStarted />}
             </Stack>
         </HomeLayout>
+    );
+}
+
+function Analytics() {
+    return (
+        <>
+            <StreamHistoryStats />
+            <StreamHistory />
+            <TopArtists />
+        </>
+    );
+}
+
+function GetStarted() {
+    return (
+        <Card variant='outlined'>
+            <Box
+                display='flex'
+                flexDirection='column'
+                alignContent='center'
+                justifyContent='center'
+                alignItems='center'
+                padding={5}
+                textAlign='center'
+            >
+                <Typography variant='h5' sx={{ margin: 1.5 }}>
+                    Get started by uploading your stream history!
+                </Typography>
+                <Typography variant='body1' sx={{ margin: 1.5 }}>
+                    Upload your stream history from Spotify.
+                </Typography>
+                <Box>
+                    <UploadFiles />
+                </Box>
+            </Box>
+        </Card>
     );
 }
