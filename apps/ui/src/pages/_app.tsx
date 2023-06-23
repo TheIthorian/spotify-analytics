7cd9dd287deeb39576164ed4b5ed52bcddc3c729
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-23 18:54:31 +0100

    feat: add themes

--- apps/ui/src/pages/_app.tsx
+++ apps/ui/src/pages/_app.tsx
@@ -3,16 +3,31 @@ import type { AppProps } from 'next/app';
 
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 // https://react-redux.js.org/tutorials/quick-start
 const store = configureStore({
     reducer: {},
 });
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#1976d2',
+        },
+    },
+});
+
+const lightTheme = createTheme({});
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
-            <Component {...pageProps} />{' '}
+            <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                <Component {...pageProps} />{' '}
+            </ThemeProvider>
         </Provider>
     );
 }
