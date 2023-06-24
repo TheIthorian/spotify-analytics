8416bbc775deece8307d7017dad7ec993388eb5f
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-24 11:30:28 +0100

    feat: add font to themes

--- apps/ui/src/pages/_app.tsx
+++ apps/ui/src/pages/_app.tsx
@@ -3,23 +3,13 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
 import { configureStore } from '@reduxjs/toolkit';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme, lightTheme } from '@/styles/themes';
 
 // https://react-redux.js.org/tutorials/quick-start
 const store = configureStore({
     reducer: {},
 });
-
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#1976d2',
-        },
-    },
-});
-
-const lightTheme = createTheme({});
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
