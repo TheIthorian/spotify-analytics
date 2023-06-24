33b155ee575540ab3fb68b7ce1cd603e62c2a1e1
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-24 12:11:14 +0100

    feat: add more info on intro page

--- apps/ui/src/pages/index.tsx
+++ apps/ui/src/pages/index.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 
-import { HomeLayout } from '@/layout/home';
 import { Inter } from 'next/font/google';
 
+import { Box, Card, Stack, Typography } from '@mui/material';
+
+import { HomeLayout } from '@/layout/home';
 import { Nav } from 'c/nav';
 import { StreamHistory } from '@/components/stream-history';
 import { TopArtists } from '@/components/top-artists';
 import { StreamHistoryStats } from '@/components/stream-history-stats';
-
-import { Box, Button, Card, Stack, Typography } from '@mui/material';
-import { CONFIG } from '@/config';
 import { UploadFiles } from '@/components/upload-file';
+
+import { CONFIG } from '@/config';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -47,45 +48,55 @@ export default function Home() {
     return (
         <HomeLayout>
             <Nav />
-            <Stack sx={{ padding: 2 }} spacing={2}>
-                {userDetails.streamHistoryRecordCount > 0 ? <Analytics /> : <GetStarted />}
-            </Stack>
+            {userDetails.streamHistoryRecordCount > 0 ? <Analytics /> : <GetStarted />}
         </HomeLayout>
     );
 }
 
 function Analytics() {
     return (
-        <>
+        <Stack spacing={2} minHeight='100%' padding={2}>
             <StreamHistoryStats />
             <StreamHistory />
             <TopArtists />
-        </>
+        </Stack>
     );
 }
 
 function GetStarted() {
     return (
-        <Card variant='outlined'>
-            <Box
-                display='flex'
-                flexDirection='column'
-                alignContent='center'
-                justifyContent='center'
-                alignItems='center'
-                padding={5}
-                textAlign='center'
-            >
-                <Typography variant='h5' sx={{ margin: 1.5 }}>
-                    Get started by uploading your stream history!
-                </Typography>
-                <Typography variant='body1' sx={{ margin: 1.5 }}>
-                    Upload your stream history from Spotify.
-                </Typography>
-                <Box>
-                    <UploadFiles />
+        <Box id='home-layout-stack' minHeight='100%' width='100%' position='absolute' top={0} bottom={0}>
+            <Card variant='outlined' sx={{ height: '100%' }}>
+                <Box
+                    display='flex'
+                    flexDirection='column'
+                    alignContent='center'
+                    justifyContent='center'
+                    alignItems='center'
+                    paddingY={10}
+                    paddingX={2}
+                    textAlign='center'
+                >
+                    <Box marginBottom={3} display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
+                        <Typography variant='h2' sx={{}}>
+                            Get Started!
+                        </Typography>
+                        <Typography variant='body1' sx={{ margin: 1.5, maxWidth: 800 }}>
+                            Upload your stream history from Spotify.
+                            <br />
+                            We&apos;ll analyse your listening habits and show you some cool charts.
+                            <br />
+                        </Typography>
+                    </Box>
+                    <Box marginBottom={1}>
+                        <UploadFiles />
+                    </Box>
+                    <Typography variant='caption' sx={{ margin: 1.5, maxWidth: 800 }}>
+                        Not sure where to get this from? Read our guide on how to download your stream history from Spotify.
+                    </Typography>
+                    <Typography variant='caption'>Read our privacy policy to learn more about how we use your data.</Typography>
                 </Box>
-            </Box>
-        </Card>
+            </Card>
+        </Box>
     );
 }
