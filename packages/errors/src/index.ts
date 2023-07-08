cb74349349381a2d12afed0a830e00946ac3cb77
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-07-08 21:14:13 +0100

    refactor: simplify error constructors

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,10 +1,11 @@
 export class AppError<T = undefined> extends Error {
     name: string;
     data: T | undefined;
+
     status = 500;
     logError = false;
 
-    constructor(message: string, name: string, opts: { data?: T; details: string }) {
+    constructor(message: string, name: string, opts: { data?: T; details?: string } = {}) {
         super(message);
         this.data = opts.data;
         this.name = name;
@@ -24,8 +25,8 @@ export class AppError<T = undefined> extends Error {
 
 export class ResourceNotFoundError extends AppError<object> {
     status = 404;
-    constructor(details: string, data: object = {}) {
-        super('Resource not found', ResourceNotFoundError.name, { data, details });
+    constructor(message: string, data: object = {}) {
+        super(message, ResourceNotFoundError.name, { data });
     }
 }
 
@@ -33,5 +34,12 @@ export class UnableToCreateResourceError extends AppError<object> {
     status = 500;
     constructor(details: string, data: object = {}) {
         super('Unable to create resource', UnableToCreateResourceError.name, { data, details });
+    }
+}
+
+export class NotImplementedError extends AppError<object> {
+    status = 501;
+    constructor(message: string) {
+        super(message, NotImplementedError.name);
     }
 }
