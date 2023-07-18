9f3c691365482f887c225e8b532c6fc7a631d0a5
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-07-18 19:27:01 +0100

    chore: remove login api method

--- apps/api/src/auth/api.ts
+++ apps/api/src/auth/api.ts
@@ -1,17 +1,11 @@
 import { NotImplementedError } from 'spotify-analytics-errors';
 import { makeLogger } from '../logger';
-
 const log = makeLogger(module);
 
 export async function webFlowLogin(provider: string) {
     log.info({ provider }, `(${webFlowLogin.name})`);
     throw new NotImplementedError(webFlowLogin.name + ' not implemented');
     return;
-}
-
-export async function login(credentials: { username: string; password: string }) {
-    log.info({ credentials }, `(${login.name})`);
-    throw new NotImplementedError(login.name + ' not implemented');
 }
 
 export async function logout(session: string) {
