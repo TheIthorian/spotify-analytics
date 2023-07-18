530135789ad2a5d192a867c03a7562b622395d28
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-07-18 19:25:22 +0100

    feat: add login handler

--- apps/api/src/auth/routes.ts
+++ apps/api/src/auth/routes.ts
@@ -1,11 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
 import expressPromiseRouter from 'express-promise-router';
-import { loginHandler, logoutHandler } from './handlers';
+import * as passport from 'passport';
 
 function init() {
     const router = expressPromiseRouter();
-    router.post('/login', loginHandler); // TODO - finish this
-    router.post('/logout', logoutHandler);
+
+    router.post('/login', [
+        passport.authenticate('local', { session: true }),
+        (req: Request, res: Response, next: NextFunction) => {
+            res.status(200);
+            res.json({ message: 'Logged in' });
+            next();
+        },
+    ]);
+
+    router.post('/logout', (req: Request, res: Response, next: NextFunction) => {
+        res.status(200);
+        res.json({ message: 'Logged in' });
+        next();
+    });
+
     // router.post('/signup', signupHandler);
+
     return router;
 }
 
