6b3c56d284646e8c4527c263e5558bedc443bea8
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-07-18 19:28:03 +0100

    feat: auth account route using passport

--- apps/api/src/account/handlers.ts
+++ apps/api/src/account/handlers.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
+import * as passport from 'passport';
 
 import { makeLogger } from '../logger';
 import * as api from './api';
@@ -6,6 +7,7 @@ import * as api from './api';
 const log = makeLogger(module);
 
 export const getUserDetailsHandler: RequestHandler[] = [
+    passport.authenticate('session'),
     async (req: Request, res: Response, next: NextFunction) => {
         try {
             const userDetails = await api.getUserDetails(1);
